6691d78b3bc6ebde66ea5a96e1f0d66f0df119ff
Christian Wilson <santiagowilson@icloud.com>
2023-03-10 18:53:58 -0500

    initial layout

---
+++ packages/ui-library/components/Button.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function Button(props: any) {
+    // return <Button onClick={() => props.onClick()}>{props.children}</Button>;
+    return <div>test</div>
+  }
+  export default Button;
